70d317ca7f504d77fa825cf4f5e90509590ce366
John Henderson <johnhenderson@fastmail.fm>
2018-07-13 16:55:41 +0000

    Styled the pages, added a delete list feature that doesn't work

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,4 +9,37 @@ class TodolistsController < ApplicationController
     @todolist = Todolist.find(params[:id])
     # shows a single to-do list
   end
+
+  def new
+    @todolist = Todolist.new
+  end
+
+  def create
+    # @todolist = Todolist.create(todolist_params.merge(user: current_user))
+
+    # or the above and then check if @todolist.valid?
+    @todolist = Todolist.new(todolist_params)
+    @todolist.user = current_user
+    if @todolist.save
+      redirect_to todolist_path(@todolist)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @todolist = Todolist.find(params[:id])
+    if @todolist.user == current_user
+      @todolist.destroy
+      redirect_to todolists_path
+    else
+      redirect_to todolists_path
+    end
+  end
+
+  private
+
+  def todolist_params
+    params.require(:todolist).permit(:name, :current_user)
+  end
 end
