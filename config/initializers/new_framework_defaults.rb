bfaa7e29e6a9f54f9ca4c8d35c3fe1414fb45fef
John Henderson <johnhenderson@fastmail.fm>
2019-05-15 16:54:58 +0000

    Updated Rails to 5.1, edited config file

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -4,7 +4,8 @@
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
-Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
+# Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
+# Commented out ^this line for Rails 5.1
 
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = true
