5aeedbb8a05ab0c8f2459654090079f9f139f0a2
John Henderson <johnhenderson@fastmail.fm>
2024-08-27 16:19:06 +0100

    Added some data to 'seeds.rb' to give the app something to display

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeds.rb text: (I've manually added 'id' numbers. I don't know if Rails will allow this.)
+
+user = User.new(
+  email: 'jack_r_henderson@yahoo.co.uk', 
+  password: 'top-secret', 
+  password_confirmation: 'top-secret'
+)
+# user.skip_confirmation!
+user.save!
+
+Todolist.create(name: 'Chores', user_id: '1')
+Todolist.create(name: 'Coding', user_id: '1')
+
+Task.create(title: 'Vacuum the carpet', done: 'false', todolist_id: '1')
+Task.create(title: 'Wash the dishes', done: 'false', todolist_id: '1')
+Task.create(title: 'Fetch the clothes', done: 'false', todolist_id: '1')
+Task.create(title: 'Get detergent', done: 'false', todolist_id: '1')
+Task.create(title: 'Do the laundry', done: 'false', todolist_id: '1')
+Task.create(title: 'Javascript', done: 'false', todolist_id: '2')
+Task.create(title: 'Codewars', done: 'false', todolist_id: '2')
+Task.create(title: 'Ruby', done: 'false', todolist_id: '2')
